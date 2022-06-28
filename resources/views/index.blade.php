c640095a5e55e04d1aea4acfc6272d888180ef7e
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-06-28 12:28:59 +0600

    Admin Panel Laravel Front end Scaffolding by React js

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Panel</title>
+
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/responsive.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/sideNav.css')}}" rel="stylesheet" >
+</head>
+<body>
+
+
+<div id="root">
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript" src="{{asset('js/Main.js')}}" ></script>
+</body>
+</html>
